e030c37e4e38ac40ff04ec214e90fa503efba22c
Rexter Delos Santos <rexterds@hawaii.edu>
2021-07-31 11:11:07 -1000

    Testing ptero server connection

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import os
 import discord
-# from pydactyl import PterodactylClient #upm package(py-dactyl)
-from keep_alive import keep_alive
+from dotenv import load_dotenv
+from pydactyl import PterodactylClient #upm package(py-dactyl)
+# from keep_alive import keep_alive
+
+# load environment variables from .env
+load_dotenv()
 
 # save secret keys to variables
-server_key = os.environ['server_key']
-panel_url = os.environ['panel_url']
+server_key = os.environ['SERVER_KEY']
+panel_url = os.environ['PANEL_URL']
 
 # connect to the discord and pterodactly clients
 discord_client = discord.Client()
-# server_client = PterodactylClient(panel_url, server_key)
+server_client = PterodactylClient(panel_url, server_key)
 
-# my_servers = server_client.client.list_servers()
-# print(my_servers)
+print('hello')
+print(discord_client)
+print(server_client)
 
 # event for when discord bot is ready
 @discord_client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(discord_client))
+
+# functions for commands
+async def hello(message):
+  await message.channel.send('Hello :)')
+  return
+
+async def hi(message):
+  await message.channel.send('Hi :)')
+  return
 
 # event for when a user sends a message
 @discord_client.event
@@ -27,9 +41,7 @@ async def on_message(message):
   if message.author == discord_client.user:
     return
   
-  if message.content.startswith('!hello'):
-    await message.channel.send('Hello :)')
-
-
-keep_alive()
+  if message.content.startswith('!'):
+    print(message.content)
+  
 discord_client.run(os.getenv('discord_key'))
