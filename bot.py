61929dd1e7680d9904d45d30ae46e9707a969899
Rexter Delos Santos <rexterds@hawaii.edu>
2021-07-31 23:32:19 -1000

    fixes to stuff the bot sends as message

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import os
-import discord
 import urllib.request
 from dotenv import load_dotenv
 from pydactyl import PterodactylClient #upm package(py-dactyl)
@@ -26,6 +25,7 @@ def get_logs():
   
   return data.decode('utf-8')
 
+# check server status
 @tasks.loop(seconds=5.0)
 async def check_server(ctx, option):
   # get current server state
@@ -33,10 +33,10 @@ async def check_server(ctx, option):
   current_state = server_utilization.get('current_state')
 
   if ((option == 'start' or option == 'restart') and current_state == 'running'):
-    await ctx.send('current state of server: ' + current_state)
+    await ctx.send('Done. Current state of server: ' + current_state)
     check_server.cancel()
   elif ((option == 'stop' or option == 'kill') and current_state == 'offline'):
-    await ctx.send('current state of server: ' + current_state)
+    await ctx.send('Done. Current state of server: ' + current_state)
     check_server.cancel()
 
 # -----------------------------------------------  BOT STUFF -----------------------------------------------------------------------
@@ -54,7 +54,9 @@ async def on_ready():
 # ----------------------------------------------- command functions ----------------------------------------------------------------
 
 # command for checking the list of players online
-@bot.command(name='list', brief='Returns the list of current online players', description='Returns the list of current online players')
+list_brief = 'Returns the list of current online players.'
+list_description = 'Returns the list of current online players.'
+@bot.command(brief=list_brief, description=list_description)
 async def list(ctx):
   # send the /list command to the server
   server_client.client.send_console_command(server_id, '/list')
@@ -71,25 +73,29 @@ async def list(ctx):
   await ctx.send(last_line_list[3])
 
 # command for sending power actions to server
-@bot.command()
+power_brief = 'Sends a power signal to the server.'
+power_description = 'Sends a power signal to the server. Use with the following options: start, restart, stop, kill (e.g. $power start). Only use kill when the server is stuck.'
+@bot.command(brief=power_brief, description=power_description)
 async def power(ctx, option):
   # check if option is one of these signals
   if (option == 'start' or option == 'restart' or option == 'stop' or option == 'kill'):
     # send the signal to the server
     server_client.client.send_power_action(server_id, option)
-    await ctx.send('sending ' + option + ' signal to server')
+    await ctx.send('Sending ' + option + ' signal to server...')
 
     # check the current state of the server
     check_server.start(ctx, option)
   else:
-    await ctx.send('Invalid option. If you need help, type $help power.')
+    await ctx.send('Invalid option. If you need brief, type $help power.')
 
 # command for checking the state of the server
-@bot.command()
+state_brief = 'Returns the current state of the server.'
+state_description = 'Returns the current state of the server. States include states such as running, stopping, starting, and offline.'
+@bot.command(brief=state_brief, description=state_description)
 async def state(ctx):
   # check the current state of the server
   server_utilization = server_client.client.get_server_utilization(server_id)
-  await ctx.send('current state of server: ' + server_utilization.get('current_state'))
+  await ctx.send('Current state of server: ' + server_utilization.get('current_state'))
 
 # start the bot
 bot.run(os.getenv('DISCORD_KEY'))
